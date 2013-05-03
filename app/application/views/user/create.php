b2a9d60b76eaf949ecaeadb4b153e081543170de
triangulito <triangulito@gmail.com>
2013-05-03 13:10:13 -0700

    Added the actions and views for new users

---
+++ app/application/views/user/create.php
@@ -0,0 +1,45 @@
+
+<div class="row">
+    <?php echo form_open(base_url('user/create_user')); ?>
+    
+            <legend>New User</legend>
+            <div class="control-group">
+                <label class="control-label" for="username">Username</label>
+                <div class="controls">
+                    <input type="text" id="username" name="username" placeholder="Username">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="password">Password</label>
+                <div class="controls">
+                    <input type="password" id="password" name="password" placeholder="Password">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="first_name">First Name</label>
+                <div class="controls">
+                    <input type="text" id="first_name" name="first_name" placeholder="First Name">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="last_name">Last Name</label>
+                <div class="controls">
+                    <input type="text" id="last_name" name="last_name" placeholder="Last Name">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="email">Email</label>
+                <div class="controls">
+                    <input type="email" id="email" name="email" placeholder="Email">
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="role">Role</label>
+                <div class="controls">
+                    <input type="text" id="role" name="role" placeholder="Role">
+                </div>
+            </div>
+            <input type="submit" class="btn" value="Create New User" />
+        </fieldset>
+    <?php echo form_close(); ?>
+</div>
