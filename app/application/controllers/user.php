89781900d6425aa5e7ef173738c77e610fee08a8
triangulito <triangulito@gmail.com>
2013-05-03 13:12:59 -0700

    Forgot to add controller changes and changes to user list view (added a button)

--- app/application/controllers/user.php
+++ app/application/controllers/user.php
@@ -20,4 +20,36 @@ class User extends CI_Controller {
 		$this->load->view('user/list', $data);
 		$this->load->view('inc/footer', $data);
 	}
+        
+        public function create()
+        {
+            $data['page_title'] = "Create New User";
+            $data['active_link'] = "user-menu";
+            $this->load->helper('form');
+            
+            $this->load->view('inc/header', $data);
+            $this->load->view('user/create', $data);
+            $this->load->view('inc/footer', $data);
+        }
+        
+        public function create_user()
+        {
+            $this->load->library('firephp');
+            $data['page_title'] = "User list";
+            $data['active_link'] = "user-menu";
+            $this->firephp->trace($_POST['username']);
+            $this->User_model->username = $_POST['username'];
+            $this->User_model->password = $_POST['password'];
+            $this->User_model->first_name = $_POST['first_name'];
+            $this->User_model->last_name = $_POST['last_name'];
+            $this->User_model->email = $_POST['email'];
+            $this->User_model->role = $_POST['role'];
+            $this->User_model->create();
+            
+            $data['users'] = $this->User_model->list_all(MAX_RECORDS_PER_PAGE, 0);
+            
+            $this->load->view('inc/header', $data);
+            $this->load->view('user/list', $data);
+            $this->load->view('inc/footer', $data);
+        }
 }
--- app/application/views/user/list.php
+++ app/application/views/user/list.php
@@ -1,25 +1,23 @@
 
-            <div class="row">
-                <input type="text" class="search search-query" placeholder="Search..."/>
-                <div class="clear-r"></div>
-                <?php
-                    $tmpl = array(
-                        'table_open' => "<table class='table table-hover'>"
-                    );
-                    $this->table->set_template($tmpl); 
-                    $this->table->set_heading('ID', 'Username', 'First Name', 'Last Name', 'Email', 'Role');
+<div class="row">
+    <input type="text" class="search search-query" placeholder="Search..."/>
+    <div class="clear-r"></div>
+    <?php
+    $tmpl = array(
+        'table_open' => "<table class='table table-hover'>"
+    );
+    $this->table->set_template($tmpl);
+    $this->table->set_heading('ID', 'Username', 'First Name', 'Last Name', 'Email', 'Role');
 
-                    foreach($users->result() as $user) {
-                        $this->table->add_row(
-                            $user->id,
-                            $user->username,
-                            $user->first_name,
-                            $user->last_name,
-                            $user->email,
-                            $user->role
-                        );
-                    }
+    foreach ($users->result() as $user) {
+        $this->table->add_row(
+                $user->id, $user->username, $user->first_name, $user->last_name, $user->email, $user->role
+        );
+    }
 
-                    echo $this->table->generate();
-                ?>  
-            </div>
+    echo $this->table->generate();
+    ?>  
+</div>
+<div class="row">
+    <a href="<?php echo base_url('user/create'); ?>" class="btn">New User</a>
+</div>
